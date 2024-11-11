2372cf33d32bade3659d8be03c51ecbdcbddbc57
Alex-Ibarra21 <alexibarra123456@outlook.com>
2024-11-11 14:55:41 -0600

    My fisrt commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8"/>
+    <title>Farmacia</title>
+    <meta name="description" content="Farmacia"/>
+    <meta name="keywords" content="Farmacia"/>
+    <meta name="author" content="Farmacia"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="./styles/style.css"/>
+  </head>
+  <body>
+    <?php
+      echo "Hola, Soy Alex Fernando Ibarra de la Vega";
+    ?>
+    
+  </body>
+</html>
